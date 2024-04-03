6199ce1879cd0bf268900327f36e1a06db13674d
pezita <nicolas.bertran@outlook.com>
2024-04-03 16:08:57 -0300

    deleted classes.scala, added a classes package, Started working on party implementation, considering Implementing abstract classes for players and  Enemies

--- src/main/scala/party/Party.scala
+++ src/main/scala/party/Party.scala
@@ -4,14 +4,14 @@ import scala.collection.mutable.ArrayBuffer
 
 class Party {
 
-  val currentParty: ArrayBuffer[Character] = new ArrayBuffer[Character];
+  val currentParty: ArrayBuffer[Character] = new ArrayBuffer[Character]
   def addCharacter(character: Character): Unit = {
     currentParty.addOne(character)
   }
 
-  def isDefeated(): Boolean = {
+  def isDefeated: Boolean = {
     //for character in party if character.hp == 0 return true (comparar y verificar todos?)
-    return false
+    false
   }
 
 
