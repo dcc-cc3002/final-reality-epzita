d96c8cc4f62780a20bdcccb51f0ceeac886cc34b
epzita <nicolas.bertran@outlook.com>
2024-07-05 23:35:10 -0400

    FEAT added some implementation

--- src/main/scala/model/state/GameState.scala
+++ src/main/scala/model/state/GameState.scala
@@ -3,9 +3,10 @@ package model.state
 class GameState(val context: GameController){
   var inPreGame:    Boolean = false
   var inGame:       Boolean = false
-  var inPlayerTurn: Boolean = false
+  var inCombat: Boolean = false
   var inEndGame:    Boolean = false
 
+  def error(): Unit = throw new Exception("This action cannot be triggered in this state")
   def setState(state: GameState): Unit = context.state = state
 
 }
--- src/main/scala/model/state/PreGame.scala
+++ src/main/scala/model/state/PreGame.scala
@@ -1,5 +1,7 @@
 package model.state
 
 class PreGame(context: GameController) extends GameState(context){
+  this.inPreGame = true
+  def startGame: Unit = this.context.state = new InGame(context)
 
 }
